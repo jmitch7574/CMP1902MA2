e749129c5cde472854326ef5287e25725ba30bb9
Jake Mitchell <j.mitchell7574@gmail.com>
2023-12-07 21:23:07 +0000

    start on task1.py

---
+++ task1.py
@@ -0,0 +1,12 @@
+"""
+This script:
+- Prompts the user to enter multiple integers
+- Saves these integers to a list
+- Calculates the amount of integers, their sum, mean, minimum and maximum
+- Finds if any numbers are prime numbers
+- Outputs this information to the terminal
+"""
+
+# Import all our math functions
+from mathsfunctions import *
+
