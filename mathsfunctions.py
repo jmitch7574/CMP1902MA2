dd77bd75eeab4d80b8240ee99384ec5df6f693b9
Jake Mitchell <j.mitchell7574@gmail.com>
2023-12-08 15:35:05 +0000

    Added more functions for prime numbers

--- mathsfunctions.py
+++ mathsfunctions.py
@@ -104,5 +104,66 @@ def maximum(list):
 
 
 """
-These Functions  are a
+These functions are all about finding any prime numbers in a given list of integers
+One function finds all the factors of a number and returns them as a list
+The other function will check if this list of factors only contains one and itself
+The prime function will return True or False
+
+You can write this as just one function that checks for factors and return false as soon
+as you find a factor that isn't 1 or itself (would be slightly more efficient). But you 
+never know when having a factors function might come in handy.
 """
+
+
+"""
+The factors function loops through all numbers between 1 and our given number
+It uses the modal function to check if our iterated number is a factor of our given number
+If the result of the modal operation is 0 we know it is a factor of our given number
+We then add this number to a list of factors
+"""
+
+def getFactors(number):
+
+    # Initialise factors list
+    factors = []
+
+    # loop through all numbers between 1 and our given number
+    for i in range(1, number + 1):
+        # Check if divisible
+        if number % i == 0:
+            # Add to the list of factors if divisible
+            factors.append(i)
+    
+    # Return our list of factors
+    return factors
+
+"""
+This function uses the getFactors function above and just checks if the only factors
+are 1 and itself
+"""
+def isPrime(number):
+    # We use a shorthand if statement to check if factors of a number are 1 and itself, return true or false
+    return True if getFactors(number) == [1, number] else False
+
+
+"""
+This function takes a list of numbers
+Iterates through the numbers
+Adds any prime numbers it finds to a list
+Returns the list
+"""
+
+def getPrimes(list):
+    # Initialise list to hold prime numbers
+    primes = []
+
+    # Iterate through numbers in our list
+    for i in list:
+        # Use our isPrime function to check if number is prime
+        if isPrime(i):
+            # add number to list if prime
+            primes.append(i)
+
+    # return prime numbers
+    return primes
+
