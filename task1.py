1657c02856a43ec7f93a89f948e7a4398bd7a693
Jake Mitchell <j.mitchell7574@gmail.com>
2023-12-08 20:43:57 +0000

    Added task 1 function

--- task1.py
+++ task1.py
@@ -10,3 +10,24 @@ This script:
 # Import all our math and input functions
 from mathsfunctions import *
 from inputfunctions import *
+
+
+inputtingNumbers = True
+numbers = []
+
+while inputtingNumbers:
+    value = inputInteger("Please enter a valid integer number, or enter 'stop' to stop entering numbers: ")
+
+    # If 'stop' is entered then inputInteger will return None
+    if value == None:
+        inputtingNumbers = False
+    
+    # A valid integer is inputted
+    else:
+        numbers.append(value)
+
+print(f"These are the {count(numbers)} numbers you entered: ")
+print(f"{number}, " for number in numbers)
+print(f"Their sum is {sum(numbers)}")
+print(f"Their mean is {mean(numbers)}")
+print(f"The list had a minimum value of {minimum(numbers)} and a maximmum of {maximum(numbers)}")
